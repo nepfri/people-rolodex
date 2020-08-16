70643e1225538f2b6a444f2a52185ae0ffd06083
fritz <fritzner.neptune1.ctr@navy.mil>
2020-08-16 13:02:02 -0700

    Updates

---
+++ precache-manifest.323b5a6559254b7cbe7943f4e3ad9717.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c87834ceceb547f4aeacc4c0c51a96eb",
+    "url": "/people-rolodex/index.html"
+  },
+  {
+    "revision": "0dd9224fc821a59f9526",
+    "url": "/people-rolodex/static/css/main.04217844.chunk.css"
+  },
+  {
+    "revision": "78b9060f1495d3485a95",
+    "url": "/people-rolodex/static/js/2.33b4488b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/people-rolodex/static/js/2.33b4488b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dd9224fc821a59f9526",
+    "url": "/people-rolodex/static/js/main.6e14d0ad.chunk.js"
+  },
+  {
+    "revision": "4aa6b8f9e8a64922ab29",
+    "url": "/people-rolodex/static/js/runtime-main.8c41662b.js"
+  }
+]);
